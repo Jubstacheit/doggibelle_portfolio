15b928e75b11f820ccaaf26409e5c15f190aa686
Viviane <dessaintviviane@gmail.com>
2024-07-02 04:05:57 +0200

    chore: Update npm dependencies and add new packages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {NextUIProvider} from "@nextui-org/react";
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Scene from "./scene";
 
 export default function Home() {
