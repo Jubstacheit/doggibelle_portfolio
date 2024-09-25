8acdc8695419ae00d996ddc501c857d5ea106283
Viviane <dessaintviviane@gmail.com>
2024-09-25 23:21:06 +0200

    Refactor CSS and update background color gradient in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Scene from "./scene";
-import colors from "@/lib/colors";
 //import Elegg from "@/components/images/Elegg";
 
 export default function Home() {
   return (
-    <section className={`h-screen w-full flex justify-center align-middle bg-pink-100 bg-[linear-gradient(to_right,${colors.backgroundGrid}_1px,transparent_1px),linear-gradient(to_bottom,${colors.backgroundGrid}_1px,transparent_1px)] bg-[size:48px_48px]`}>
+    <section className={`h-screen w-full flex justify-center align-middle bg-indigo-950 bg-[linear-gradient(to_right,#7c3a8790_2px,transparent_2px),linear-gradient(to_bottom,#7c3a8790_2px,transparent_2px)] bg-[size:48px_48px]`}>
       <Scene/>
     </section>
   );
--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,5 +1,6 @@
 const colors = {
 	backgroundGrid: '#7c3a8780',
+	backgroundGridPattern: '#7c3a8780'
 }
 
 export default colors
