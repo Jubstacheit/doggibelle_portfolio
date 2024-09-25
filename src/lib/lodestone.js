dfbc349cc1c3541b384077a653c26ede25699137
Viviane <dessaintviviane@gmail.com>
2024-09-25 03:46:15 +0200

    feat: Add lodestone component and update npm dependencies
    This commit adds a new component called "lodestone" to the project. The component renders a div with the text "lodestone" and imports React. Additionally, the commit updates the npm dependencies by adding the packages "axios", "cheerio", and "puppeteer".

---
+++ src/lib/lodestone.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function lodestone() {
+	const lodestoneUrl = "https://fr.finalfantasyxiv.com/lodestone/character/19458625/"
+  return (
+	<div>lodestone</div>
+  )
+}
+
+export default lodestone
