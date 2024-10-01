539e834447480dfb9c9d1fd80b59dfeda268dd70
Viviane <dessaintviviane@gmail.com>
2024-10-01 05:00:32 +0200

    Refactor package.json and package-lock.json to update @next/env to version 14.2.13

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -2,18 +2,14 @@ import React from "react";
 import Scene from "@/components/misc/scene";
 import Elegg from "@/components/images/Elegg";
 import Upload from "@/components/cloudinary/Upload";
-import Image from "@/components/cloudinary/Image";
-import Clock from "@/components/misc/Clock";
 
 export default async function Home() {
 
   return (
     <section className={`h-screen w-full flex justify-center align-middle bg-indigo-950 bg-[linear-gradient(to_right,#7c3a8790_2px,transparent_2px),linear-gradient(to_bottom,#7c3a8790_2px,transparent_2px)] bg-[size:48px_48px]`}>
-      
       <Scene/>
       <Elegg/>
       <Upload/>
-      <Image/>
     </section>
   );
 }
--- src/components/cloudinary/Upload.jsx
+++ src/components/cloudinary/Upload.jsx
@@ -4,17 +4,7 @@ import { CldUploadButton, CldUploadWidget } from 'next-cloudinary';
 import { Button } from '../ui/button';
 
 export default function Upload() {
-	const uploadPreset = process.env.UNSIGN_UPLOAD_PRESET
-
 	return (
-		<CldUploadWidget uploadPreset="TEMP">
-			{({ open }) => {
-				return (
-					<Button onClick={() => open()}>
-						<div className='font-heading'>Upload an Image</div>
-					</Button>
-				);
-			}}
-		</CldUploadWidget>
+		<div>test</div>
 	);
 }
