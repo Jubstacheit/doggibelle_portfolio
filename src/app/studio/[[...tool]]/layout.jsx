269087a308b3f41de667a4b1a2be92ad2fda3db1
Viviane <dessaintviviane@gmail.com>
2024-10-12 04:51:15 +0200

    Refactor RootLayout component to remove NextUIProvider and unused imports

--- src/app/studio/[[...tool]]/layout.jsx
+++ src/app/studio/[[...tool]]/layout.jsx
@@ -1,20 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import "./globals.css";
-import {NextUIProvider} from "@nextui-org/react";
-
-
 
 export default function RootLayout({ children }) {
 
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body>
-				<NextUIProvider>
-					<main>
-						{children}
-					</main>
-				</NextUIProvider>
+				<main>
+					{children}
+				</main>
 			</body>
 		</html>
 	);
