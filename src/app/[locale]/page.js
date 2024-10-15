774a8431ddacf05debe87780b01e182b039629f6
Viviane <dessaintviviane@gmail.com>
2024-10-15 17:57:04 +0200

    Refactor page.js to add Grid component

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Elegg from "@/components/images/Elegg";
 import Upload from "@/components/cloudinary/Upload";
+import Grid from "@/components/images/Grid";
 
 export default async function Home() {
   return (
     <section className={`h-screen w-full flex justify-center align-middle bg-indigo-950 bg-[linear-gradient(to_right,#7c3a8790_2px,transparent_2px),linear-gradient(to_bottom,#7c3a8790_2px,transparent_2px)] bg-[size:48px_48px]`}>
       <Elegg/>
       <Upload/>
+      <Grid/>
     </section>
   );
 }
---
+++ src/components/images/Grid.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Grid() {
+  return (
+	<div>
+		<div>
+			<div>
+			
+			</div>
+		</div>
+
+		<div>
+			<div>
+			
+			<div>Yippee!</div>
+			
+			</div>
+		</div>
+	</div>
+
+  )
+}
