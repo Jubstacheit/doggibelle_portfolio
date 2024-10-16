3ee5839aaa20ffec3634a7fbbd69e24459acabce
Viviane <dessaintviviane@gmail.com>
2024-10-16 03:59:52 +0200

    Refactor package.json and package-lock.json to remove muuri dependency

--- src/components/images/Grid.jsx
+++ src/components/images/Grid.jsx
@@ -1,21 +1,8 @@
+"use client"
 import React from 'react'
 
 export default function Grid() {
 	return (
-		<div>
-			<div>
-				<div>
-				
-				</div>
-			</div>
-
-			<div>
-				<div>
-				
-				<div>Yippee!</div>
-				
-				</div>
-			</div>
-		</div>
-	)
+		null
+	);
 }
