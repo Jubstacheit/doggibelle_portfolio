87821df0eee2e1be5c27fc9b2968c038afeb3ae5
Viviane <dessaintviviane@gmail.com>
2024-10-18 05:33:39 +0200

    Refactor package.json and package-lock.json to update eslint-plugin-tailwindcss version

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/no-contradicting-classname */
 import React from "react";
 import Elegg from "@/components/images/Elegg";
 import Upload from "@/components/cloudinary/Upload";
@@ -5,7 +6,6 @@ import Upload from "@/components/cloudinary/Upload";
 export default async function Home() {
   const text = "DOGGIBELLE";
   return (
-    // eslint-disable-next-line tailwindcss/no-contradicting-classname
     <section className={`flex h-screen w-full justify-center bg-indigo-950 bg-[linear-gradient(to_right,#7c3a8790_1px,transparent_1px),linear-gradient(to_bottom,#7c3a8790_2px,transparent_2px)] bg-[size:32px_32px] align-middle`}>
       <Elegg loop={false} src="/BOOM.mp3"/>
       <Upload/>
