d828bf5667ad73cab0ccbda78bc417fbc6b40b31
Viviane <dessaintviviane@gmail.com>
2024-10-18 03:58:55 +0200

    Refactor eslint.config.mjs to add TypeScript and TailwindCSS linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tailwind from "eslint-plugin-tailwindcss";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
 
 
 export default [
+  // add eslint built-in
+  js.configs.recommended,
+  // add `typescript-eslint` flat config simply
+  // if you would like use more another configuration,
+  // see the section: https://typescript-eslint.io/getting-started#details
+  ...ts.configs.recommended,
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReactConfig,
+  ...tailwind.configs["flat/recommended"]
 ];
