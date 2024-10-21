9b8ec980afb5678e23f43cd7ed77c2a2d5ce0001
Viviane <dessaintviviane@gmail.com>
2024-10-21 04:40:51 +0200

    Refactor package.json and package-lock.json to update eslint-plugin-tailwindcss version

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Elegg from "@/components/images/Elegg";
 import Upload from "@/components/cloudinary/Upload";
+import Link from "next/link";
+import HomeLinks from "@/components/misc/HomeLinks";
 
 export default async function Home() {
   const text = "DOGGIBELLE";
@@ -8,7 +10,11 @@ export default async function Home() {
     <section className={`flex h-screen w-full justify-center bg-indigo-950 bg-[linear-gradient(to_right,#7c3a8790_1px,transparent_1px),linear-gradient(to_bottom,#7c3a8790_2px,transparent_2px)] bg-[size:32px_32px] align-middle`}>
       <Elegg loop={false} src="/BOOM.mp3"/>
       <Upload/>
-      <p className="absolute bottom-0 left-0 m-2 flex select-none flex-col whitespace-pre font-base text-3xl leading-tight text-main">
+      <HomeLinks/>
+      <Link href="https://doggibelle.atabook.org" className="absolute top-0 m-2 flex select-none flex-col whitespace-pre text-center font-heading text-3xl leading-tight text-main">
+        Leave a comment!
+      </Link>
+      <p className="absolute bottom-0 left-0 m-2 flex select-none flex-col whitespace-pre text-center font-heading text-3xl leading-tight text-main">
         {text.split('').map((letter, index) => (
           <span key={index}>{letter}</span>
         ))}
---
+++ src/components/misc/HomeLinks.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
+
+export default function HomeLinks() {
+	return (
+		<Card className="absolute right-0 self-center w-[350px] h-20">
+			<CardHeader>
+				<CardTitle>Useful links</CardTitle>
+			</CardHeader>
+				<CardContent>
+					
+				</CardContent>
+		</Card>
+	)
+}
